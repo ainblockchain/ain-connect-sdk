d4974c448624cb26acaa3ff8bc9d4f866206b72b
Seonghwa Yun <seonghwa.yun@comcom.ai>
2020-08-28 17:11:03 +0900

    add functions

--- src/models/client.ts
+++ src/models/client.ts
@@ -21,16 +21,33 @@ export default class Client {
     this.sendTx = this.firebase.getFunctions().httpsCallable('sendTransaction');
   }
 
-  public async createResource(params: any, callback: any) {
+  private async sendRequest(type: string, params: any, callback: any) {
     const data = this.wallet.signaturePayload(params);
-    const { targetAddress, targetClusterName } = params.payload;
+    const { clusterAddress, clusterName } = params.payload;
     const requestId = getRandomRequestId();
-    const refPath = `/worker/${targetAddress}/${targetClusterName}/request_queue/${requestId}`;
-    await this.sendTx({ dbpath: refPath, ...data });
+    const refPath = `/worker/${clusterAddress}/${clusterName}/request_queue/${requestId}`;
+
+    await this.sendTx({ type, dbpath: refPath, ...data });
     this.firebase.getDatabase().ref(`${refPath}/response`)
       .on('value', (snapshot) => {
         this.firebase.getDatabase().ref(`${refPath}/response`).off();
         callback(snapshot.val());
       });
+  }
+
+  public async createResource(params: any, callback: any) {
+    this.sendRequest('createResource', params, callback);
+  }
+
+  public async deleteResource(params: any, callback: any) {
+    this.sendRequest('deleteResource', params, callback);
+  }
+
+  public async getResourceStatus(params: any, callback: any) {
+    this.sendRequest('getResourceStatus', params, callback);
+  }
+
+  public async setResourceConfig(params: any, callback: any) {
+    this.sendRequest('setResourceConfig', params, callback);
   }
 }
