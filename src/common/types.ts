0d1001c22142d1253c77bbe020ea9b7c23fa586f
Donghyeon Choi <drea123@naver.com>
2020-08-31 10:58:39 +0900

    fix: fix types and remove listenReqeust method

--- src/common/types.ts
+++ src/common/types.ts
@@ -2,11 +2,140 @@ export type ClusterRegisterParams = {
   clusterName: string;
   clusterTitle: string;
   clusterDescription: string;
-  price: number;
+  isSingleNode: 0 | 1;
   isPrivate: 0 | 1;
-  allowAddress?: string[];
-  isHttps: 0 | 1;
-  domain?: string;
-  isSubdomainMethod: 0 | 1;
-  clusterType: 'k8s' | 'docker';
+  allowAddressList?: string[]
+  endpointConfig: {
+    https: 0 | 1;
+    domainName?: string
+    ip?: string
+    istio: 0 | 1;
+  }
+  nodePool: {
+    nodePoolName: string
+    hwConfig: {
+      gpu: 0 | 1;
+      storage: 0 | 1;
+    }
+    priceConfig: {
+      cpuPerCore: number;
+      memoryPerGb: number;
+      gpu: number;
+      storagePerGb: number;
+    }
+  }
+}
+
+export type CreateResourceParams = {
+  clusterAddress: string;
+  clusterName: string;
+  type: 'deploy' | 'redeploy' | 'storage';
+  deployTemplateName?: string
+  selectClusterOption?: {
+    isSingleNode: boolean;
+    isPrivate: boolean;
+    https: boolean;
+    istio: boolean;
+    hwSpec: {
+      isGpu?: boolean;
+      isStorage?: boolean;
+    };
+  }
+  containerInfo: {
+    imageName: string;
+    nodePoolName?: string;
+    storageId?: string;
+    imageRegistryLoginInfo?: {
+      url: string;
+      id: string;
+      pw: string;
+    };
+    hwSpec: {
+      cpuPerCore: number;
+      memoryPerGb: number;
+      gpu: number;
+      storagePerGb?: number;
+    }
+    replicas?: number;
+    command?: string;
+    env?: object;
+    port: object;
+  }
+  requestTimeout?: number;
+  runningTimeout?: number;
+}
+
+export type CreateResourceReturn = {
+  statusCode: number;
+  containerId: string;
+  storageId?: string;
+  endpoint?: string;
+}
+
+export type DeleteResourceParams = {
+  clusterAddress: string;
+  clusterName: string;
+  type: 'deploy' | 'storage';
+  id: string;
+}
+
+export type GetResourceStatusParams = {
+  clusterAddress: string;
+  clusterName: string;
+  type: 'deploy' | 'storage';
+  id: string;
+}
+
+export type GetResourceStatusReturn = {
+  statusCode: number
+  containerInfo?: {
+    containerImage: string
+    port: object;
+    env: object;
+    command: string;
+  }
+  resourceStatus: number;
+}
+
+export type SetResourceConfigParams = {
+  clusterAddress: string;
+  clusterName: string;
+  containerId: string;
+  config: {
+    port?: object;
+    replicas?: number;
+    env?: object;
+  }
+}
+
+export type GetClusterInfoParams = {
+  workerAddress?: string;
+  workerName?: string;
+  clusterAddress?: string
+  clusterName?: string
+  option: {
+    isSingleNode: boolean
+    isPrivate: boolean
+    https: boolean
+    istio: boolean
+    hwSpec: {
+      isGpu?: boolean
+      isStorage?: boolean
+    }
+  }
+}
+
+export type GetHistoryParams = {
+  address: string;
+}
+
+export type GetHistoryReturn = {
+  createdAt: string;
+  finishedAt: string;
+  requestId: string;
+  clusterName: string;
+  workerAdress: string;
+  price: number;
+  statusCode: number;
+  reverseAmount: string;
 }
--- src/models/worker.ts
+++ src/models/worker.ts
@@ -1,21 +1,13 @@
 import Wallet from './wallet';
 import Firebase, { EnvType } from '../common/firebase';
 import * as types from '../common/types';
-import * as error from '../common/error';
 
 export default class Worker {
   private wallet: Wallet;
 
   private firebase: Firebase;
 
-  private methods: {[type: string]: Function};
-
-  private listenDict: {[clusterName: string]: {
-    requestRef: any,
-  }}
-
   constructor(mnumonic: string, env: EnvType) {
-    this.listenDict = {};
     this.wallet = new Wallet(mnumonic);
     this.firebase = new Firebase(env);
   }
@@ -27,49 +19,6 @@ export default class Worker {
       dbpath,
     };
     await this.firebase.getInstance().functions().httpsCallable('sendTransaction')(reqMassage);
-  }
-
-  public listenReqeust(methods: {[type: string]: Function}) {
-    this.methods = methods;
-    const rootRef = this.firebase.getInstance().database()
-      .ref(`/worker/${this.wallet.getAddress()}`);
-    rootRef
-      .on('child_added', (data) => {
-        const clusterName = data.key as string;
-
-        const requestRef = this.firebase.getInstance().database()
-          .ref(`/worker/${this.wallet.getAddress()}/${clusterName}/request_queue`);
-        this.listenDict[clusterName] = {
-          requestRef,
-        };
-
-        requestRef
-          .on('child_added', async (requestData) => {
-            const requstId = requestData.key as string;
-            const requestValue = requestData.val();
-            const dbpath = `/worker/${this.wallet.getAddress()}/${clusterName}/request_queue/${requstId}/response`;
-            if (requestValue.response) {
-              return;
-            }
-            if (this.methods[requestValue.type]) {
-              const result = await this.methods[requestValue.type](requestValue);
-              await this.sendResponse(result, dbpath);
-            } else {
-              await this.sendResponse({
-                statusCode: error.STATUS_CODE.invalidParams,
-              }, dbpath);
-            }
-          });
-      });
-
-    rootRef
-      .on('child_removed', (data) => {
-        const clusterName = data.key as string;
-        if (this.listenDict[clusterName]) {
-          this.listenDict[clusterName].requestRef.onDisconnect();
-          delete this.listenDict[clusterName];
-        }
-      });
   }
 
   public async registerCluster(option: types.ClusterRegisterParams) {
