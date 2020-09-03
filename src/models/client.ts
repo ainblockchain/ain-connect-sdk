6a4e4c6e3f4727b9a9e0e6ceaf4e7348b9e10e18
Seonghwa Yun <seonghwa.yun@comcom.ai>
2020-09-03 18:49:23 +0900

    update params type

--- src/models/client.ts
+++ src/models/client.ts
@@ -1,7 +1,7 @@
 import { customAlphabet } from 'nanoid';
 import Firebase from '../common/firebase';
 import Wallet from './wallet';
-import { EnvType } from '../common/types';
+import * as types from '../common/types';
 
 function getRandomRequestId() {
   const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 25);
@@ -15,7 +15,7 @@ export default class Client {
 
   private sendTx: any;
 
-  constructor(mnemonic: string, type: EnvType) {
+  constructor(mnemonic: string, type: types.EnvType) {
     this.wallet = new Wallet(mnemonic, type);
     this.firebase = new Firebase(type);
     this.sendTx = this.firebase.getFunctions().httpsCallable('sendTransaction');
@@ -42,27 +42,27 @@ export default class Client {
     return res;
   }
 
-  public async deploy(params: any) {
+  public async deploy(params: types.DeployParams) {
     const res = await this.sendRequest('deploy', params);
     return res;
   }
 
-  public async redeploy(params: any) {
+  public async redeploy(params: types.RedeployParams) {
     const res = await this.sendRequest('redeploy', params);
     return res;
   }
 
-  public async createStorage(params: any) {
+  public async createStorage(params: types.CreateStorageParams) {
     const res = await this.sendRequest('createStorage', params);
     return res;
   }
 
-  public async deleteStorage(params: any) {
+  public async deleteStorage(params: types.DeleteStorageParams) {
     const res = await this.sendRequest('deleteStorage', params);
     return res;
   }
 
-  public async getContainerConfig(params: any) {
+  public async getContainerConfig(params: types.GetContainerInfoParams) {
     const res = await this.sendRequest('getContainerConfig', params);
     return res;
   }
@@ -72,7 +72,7 @@ export default class Client {
     return res;
   }
 
-  public async getClusterInfo(params: any) {
+  public async getClusterInfo(params: types.GetClusterInfoParams) {
     const { targetAddress, clusterName } = params;
     const snap = await this.firebase.getDatabase().ref(`/worker/info/${clusterName}@${targetAddress}`).once('value');
 
@@ -83,7 +83,7 @@ export default class Client {
     }
   }
 
-  public async getClusterList(params: any) {
+  public async getClusterList(params: types.GetClusterListParams) {
     let list = [];
     const refPath = '/worker/info/';
     if (params.targetAddress) {
@@ -92,7 +92,7 @@ export default class Client {
         .orderByChild('address').equalTo(params.targetAddress)
         .once('value');
       list = snap.val();
-    } else if (params.clusterOptions) {
+    } else if (params.clusterOption) {
       // TODO: filtered by options
       const snap = await this.firebase.getDatabase().ref(refPath).once('value');
       list = snap.val();
@@ -104,7 +104,7 @@ export default class Client {
     return list;
   }
 
-  public async getHistory(params: any) {
+  public async getHistory(params: types.GetHistoryParams) {
     const historyList: any[] = [];
     /* TODO */
     return historyList;
