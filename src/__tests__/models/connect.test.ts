3437deaef268b410dc689014aeeda6a773abb9e4
Seonghwa Yun <seonghwa.yun@comcom.ai>
2022-01-12 19:12:33 +0900

    remove test

--- src/__tests__/models/connect.test.ts
+++ src/__tests__/models/connect.test.ts
@@ -1,33 +1,3 @@
-import Connect from '../../models/connect';
-import { NetworkType } from '../../common/types';
-
-const TEST_MNEMONIC = 'online congress divert fee canal snake make almost tube rent color jewel';
-
 describe('connect', () => {
   it('init', () => expect(true).toEqual(true));
-
-  describe('BLOCKCHAIN TEST', () => {
-    let connect: Connect;
-    beforeAll(() => {
-      connect = new Connect(NetworkType.TESTNET, TEST_MNEMONIC);
-    });
-
-    it('get value from non-exist path', async () => {
-      try {
-        const res = await connect.get('/this/is/not/exist/path');
-        expect(res).toBeNull();
-      } catch (e) {
-        fail(e);
-      }
-    });
-
-    it('get value from exist path', async () => {
-      try {
-        const res = await connect.get('/token');
-        expect(res).not.toBeNull();
-      } catch (e) {
-        fail(e);
-      }
-    });
-  });
 });
